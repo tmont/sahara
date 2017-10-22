c959c1485d5019d5f32ad7ae6851020085c21913
tmont <tommy.mont@gmail.com>
2017-10-22 01:01:14 -0700

    removed redundant index

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = require('./src');
+const injection = require('./src/injection');
+const lifetime = require('./src/lifetime');
+const Container = require('./src/container');
+const ObjectBuilder = require('./src/object-builder');
+
+module.exports = {
+	Container,
+	ObjectBuilder,
+	inject: {
+		propertyValue: (name, value) => new injection.PropertyValue(name, value),
+		property: (name, key) => new injection.Property(name, key),
+		method: (name, args) => new injection.Method(name, args)
+	},
+	lifetime: {
+		transient: () => new lifetime.Transient(),
+		memory: () => new lifetime.Memory()
+	}
+};
